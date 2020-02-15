b318f90b8bc94daf063828e457e7ca5f044b4f3e
Chau-Truong-Phat <chautruongphat123@gmail.com>
2020-02-15 12:16:37 +0700

    fix deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ module.exports = {
   },
 
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/phattrienungdung/' // Thay tên repository của các bạn vào đây nhé
+    ? '/phattrientuonglai/' // Thay tên repository của các bạn vào đây nhé
     : '/'
 }
 
